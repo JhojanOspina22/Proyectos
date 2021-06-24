2fa5ec65d3fb25c09ddf33de526ef7e6dd65dd10
Jhojan_Ospina <jc.kmilo22@gmail.com>
2021-06-24 15:12:38 -0500

    push
    se sube proyecto sweet surprise

---
+++ prueba/Repositorio/UsuarioRepository.cs
@@ -0,0 +1,107 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+
+namespace Repositorio
+{
+    public class UsuarioRepository
+    {
+        public List<Usuario> GetUsuarios()
+        {
+            using(var ctx= new pruebaContainer ())
+            {
+                return ctx.UsuarioSet.Select(u =>  new Usuario { 
+                    IdUsuario=u.IdUsuario,
+                    NombreCompleto=u.NombreCompleto,
+                    Identificacion=u.Identificacion,
+                    Telefono=u.Telefono,
+                    Direccion=u.Direccion,
+                    Ciudad=u.Ciudad,
+                    Correo=u.Correo
+
+                }).ToList();
+            }
+        }
+
+        public Usuario ListarUsuario(int IdUsuario)
+        {
+            using (var ctx = new pruebaContainer())
+            {
+                return ctx.UsuarioSet.Select(u => new Usuario
+                {
+                    IdUsuario = u.IdUsuario,
+                    NombreCompleto = u.NombreCompleto,
+                    Identificacion = u.Identificacion,
+                    Telefono = u.Telefono,
+                    Direccion = u.Direccion,
+                    Ciudad = u.Ciudad,
+                    Correo = u.Correo
+
+                }).FirstOrDefault(u => u.IdUsuario == IdUsuario);
+            }
+        }
+
+        public bool CrearUsuarios(Usuario u)
+        {
+            using (var ctx = new pruebaContainer())
+            {
+             
+                ctx.UsuarioSet.Add(new UsuarioSet
+                {
+                    IdUsuario=u.IdUsuario,
+                    NombreCompleto=u.NombreCompleto,
+                    Identificacion=u.Identificacion,
+                    Telefono=u.Telefono,
+                    Direccion=u.Direccion,
+                    Ciudad=u.Ciudad,
+                    Correo=u.Correo
+                });
+                ctx.SaveChanges();
+
+                return true;
+            }
+        }
+
+        public bool ModificarUsuarios(Usuario u)
+        {
+            using (var ctx = new pruebaContainer())
+            {
+
+                var usuario = ctx.UsuarioSet.FirstOrDefault(uu => uu.IdUsuario == u.IdUsuario);
+
+                if (usuario == null)
+                    return false;
+
+                    usuario.NombreCompleto = u.NombreCompleto;
+                    usuario.Identificacion = u.Identificacion;
+                    usuario.Telefono = u.Telefono;
+                    usuario.Direccion = u.Direccion;
+                    usuario.Ciudad = u.Ciudad;
+                    usuario.Correo = u.Correo;
+
+                ctx.SaveChanges();
+
+                return true;
+            }
+        }
+        public bool EliminarUsuarios(Usuario u)
+        {
+            using (var ctx = new pruebaContainer())
+            {
+
+                var usuario = ctx.UsuarioSet.FirstOrDefault(uu => uu.IdUsuario == u.IdUsuario);
+
+                if (usuario == null) return false;
+
+                ctx.UsuarioSet.Remove(usuario);
+                ctx.SaveChanges();
+
+                return true;
+            }
+        }
+    }
+}
---
+++ prueba/prueba/Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Entidades;
+using Logica;
+namespace prueba.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+           var logica = new UsuarioLogica();
+
+            return View(logica.GetUsuarios());
+        }
+
+        [HttpPost]      
+        public ActionResult Index( HttpPostedFileBase archivo)
+        {
+          
+            var logica = new UsuarioLogica();
+            StreamReader sr = new StreamReader(archivo.InputStream);
+            var csv= sr.ReadToEnd();
+            foreach (string linea in csv.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(linea))
+                {
+                    var valores = linea.Split(',');
+
+                    logica.CrearUsuarios(new Usuario
+                    {
+                        NombreCompleto = valores[0].Trim(),
+                        Identificacion = valores[1].Trim(),
+                        Telefono = valores[2].Trim(),
+                        Direccion = valores[3].Trim(),
+                        Ciudad = valores[4].Trim(),
+                        Correo = valores[5].Trim(),
+                    });
+                }
+            }
+            sr.Close();
+            
+            return View(logica.GetUsuarios());
+        }
+
+        [HttpGet]
+        public ActionResult EditarUsuario(int IdUsuario)
+        {
+
+            var logica = new UsuarioLogica();
+            return View(logica.ListarUsuario(IdUsuario));
+        }
+
+        [HttpPost]
+        public ActionResult EditarUsuario(Usuario u)
+        {
+
+            var logica = new UsuarioLogica();
+            logica.ModificarUsuarios(u);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult EliminarUsuario(int IdUsuario)
+        {
+
+            var logica = new UsuarioLogica();
+            logica.EliminarUsuarios(new Usuario
+            {
+                IdUsuario = IdUsuario
+
+            }) ;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ExportarXls()
+        {
+
+            var logica = new UsuarioLogica();
+            logica.ExportarXls(logica.GetUsuarios(), Response);
+            return RedirectToAction("Index");
+        }
+        public FileResult ExportarCsv()
+        {
+
+            var logica = new UsuarioLogica();
+          
+
+            return File(Encoding.UTF8.GetBytes(logica.ExportarCsv(logica.GetUsuarios())), "text/csv", "Informe.csv");
+        }
+
+    }
+}
